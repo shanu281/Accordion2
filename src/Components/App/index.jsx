bd8605d2ed607e8b1d4ec1fb532d4b31d5fe2f39
Shanu <shanukumar@shanus-MacBook-Air.local>
2022-11-14 10:20:49 +0530

    hand pointer added

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -6,14 +6,12 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            active : ""
+            active: ""
         }
     }
     handleClick = (i) => {
-this.setState({
-    active : i
-})
-console.log(i)
+        this.setState({ active: this.state.active === i ? "" : i })
+        console.log(i)
     }
     render() {
         console.log(faqs[1].Q)
@@ -23,11 +21,20 @@ console.log(i)
                     <div className="faq-container">
                         {
                             faqs.map((faq, i) =>
-                                <div className={this.state.active === i? "active" : "faq"} onClick={() => this.handleClick(i)}>
-
-                                    <h2>{faqs[i].Q}</h2>
-                                    <span><i class="fa-solid fa-hand-point-down"></i></span>
-
+                                <div onClick={() => this.handleClick(i)}>
+                                    <div>
+                                        <div className="faq ">
+                                            <h2>{faqs[i].Q}</h2>
+                                            <span>
+                                                {
+                                                    this.state.active === i ?  <i class="fa-sharp fa-solid fa-hand-point-up"></i> : <i class="fa-solid fa-hand-point-down"></i> 
+                                                }
+                                            </span>
+                                        </div>
+                                        <div className={this.state.active === i ? "active" : "hidden"}>
+                                            <h2>{faqs[i].A}</h2>
+                                        </div>
+                                    </div>
                                 </div>
                             )
                         }
